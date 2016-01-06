2b1591af255d40535c1302d06f8af27ce13926bf
Jeffrey Torres <torres.jeff@hotmail.com>
2016-01-06 15:01:33 -0500

    13B_ModelImporting

--- 13B_ModelImporting/ModelImportingApplication.cpp
+++ 13B_ModelImporting/ModelImportingApplication.cpp
@@ -13,11 +13,14 @@ ModelImportingApplication::~ModelImportingApplication()
 void ModelImportingApplication::Initialize()
 {
 	FirstPersonApplication::Initialize();
-
+	
 	//mModelComponent = new ModelImportingComponent(*this, *mCamera, "res/nanosuit.obj", Shader("shaders/model.vert", "shaders/model.frag"));
 	mModelComponent = new ModelImportingComponent(*this, *mCamera);
 	mComponents.push_back(mModelComponent);
+	
+	//Wireframe mode
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	glEnable(GL_DEPTH_TEST);
 	Application::Initialize();
 }
